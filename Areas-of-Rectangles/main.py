1d6b6fb7197f19def53f965fded9d5b39ce90a5e
Hunter2552 <hunterrichardson@westernwyoming.edu>
2024-09-16 20:50:59 -0700

    Add files via upload

--- Areas-of-Rectangles/main.py
+++ Areas-of-Rectangles/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Hunter Richardson
+# 9/16/2024
 # Areas of Rectangles Programming Project
 # COSC 2409 DNT
 #
@@ -19,3 +19,23 @@
 # Calculate area B
 
 # Print area comparison using if-elif-else statements
+
+# Recatangle 1
+Length1=int(input('Length of rectangle 1: '))
+Width1=int(input('Width of rectangle 1: '))
+# Recatangle 2
+Length2=int(input('Length of rectangle 2: '))
+Width2=int(input('Width of rectangle 2: '))
+
+#Area
+Area1=Length1*Width1
+Area2=Length2*Width2
+
+# <,=,> of Area 1 and 2
+if Area1>Area2:
+    print('Rectangle 1 has a greater area.')
+elif Area2 > Area1:
+    print('Rectangle 2 has a greater area.')
+else:
+    print('Both have the same area')
+    
