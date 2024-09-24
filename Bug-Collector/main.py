d2ba91847581a9612d6a6af0b0bcd537e5d14c7a
Hunter2552 <hunterrichardson@westernwyoming.edu>
2024-09-23 21:51:46 -0700

    Add files via upload

--- Bug-Collector/main.py
+++ Bug-Collector/main.py
@@ -1,6 +1,6 @@
 #
-# Name
-# Date
+# Hunter Richardson
+# 9/23/2024
 # Bug Collector Programming Project
 # COSC 2409 DNT
 #
@@ -9,3 +9,20 @@
 # Get number of bugs collected each day using a for loop.
 
 # Display the total number of bugs collected.
+
+#initialize the accumulator
+total=0
+
+# Get the bugs collected for each day
+for day in range (1, 8):
+    # Prompt the user
+    print('Enter bugs collected on day', day)
+
+    # Input the number of bugs
+    bugs = int(input())
+
+    # Add bugs to total
+    total+=bugs
+
+# Display the total bugs.
+print('You collected a total of', total, 'bugs.' )
