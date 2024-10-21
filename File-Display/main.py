f3852534eb7df2abd535d50b21a26024e092cd02
Hunter2552 <hunterrichardson@westernwyoming.edu>
2024-10-21 10:41:51 -0700

    Add files via upload

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,22 @@
 #
-# Name
-# Date
+# Hunter Richardson
+# 10/20.2024
 # File Display Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+# Open the file
+# For each line open the file: Read the line, display it, and then close it.
+
+#open the file.
+myfile=open('numbers.txt' , 'r')
+
+
+#Read and displays the file's content.
+for line in myfile:
+    number=int(line)
+    print(number)
+
+#Step 3: Close the file
+myfile.close()
