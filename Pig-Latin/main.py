13e5614af7ad6bdf54205545d046b8fdba4125a0
Hunter2552 <hunterrichardson@westernwyoming.edu>
2024-12-04 11:49:37 -0700

    Add files via upload

--- Pig-Latin/main.py
+++ Pig-Latin/main.py
@@ -1,7 +1,35 @@
 #
-# Name
-# Date
+# Hunter richardson
+# 12/4/2024
 # Pig Latin Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+def to_pig_latin(word):
+    # Convert the word to Pig Latin
+    return word[1:] + word[0].lower() + "ay"
+
+
+def main():
+    # Sentence from the user
+    sentence = input("Enter a sentence: ")
+    
+
+
+    # Split the sentence into words
+    words = sentence.split()
+    
+    # Convert func.
+    pig_latin_words = [to_pig_latin(word) for word in words]
+    
+    # words->sentence
+    pig_latin_sentence = " ".join(pig_latin_words)
+    
+    # Print pig Latin
+    print("Pig Latin:", pig_latin_sentence)
+
+
+
+# Call the MAIN
+main()
